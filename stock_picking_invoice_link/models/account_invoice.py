e1f24ba2d8dbd2d6635c0f0c8b5eb5101e83ee7c
catalin <catalin@domatix.com>
2019-08-19 13:48:06 +0200

    [ADD] stock_picking_invoice_link: stock picking reports as attachments to the email created by the `Send by Email` button`

--- stock_picking_invoice_link/models/account_invoice.py
+++ stock_picking_invoice_link/models/account_invoice.py
@@ -5,6 +5,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import api, fields, models
+import base64
 
 
 class AccountInvoice(models.Model):
@@ -29,6 +30,26 @@ class AccountInvoice(models.Model):
                         result[i][2][name] = [(6, 0, line[name].ids)]
                         line[name] = False
         return result
+    def action_invoice_sent(self):
+        self.ensure_one()
+        attachment_ids = list()
+        for picking_id in self.picking_ids:
+            pdf = self.env.ref('stock.action_report_picking') \
+                .render_qweb_pdf([picking_id.id])[0]
+            attachment_ids.append(self.env['ir.attachment'].create({
+                'name': picking_id.display_name,
+                'type': 'binary',
+                'res_id': picking_id.id,
+                'res_model': 'stock.picking',
+                'datas': base64.b64encode(pdf),
+                'mimetype': 'application/x-pdf',
+                'datas_fname': "%s.pdf" % picking_id.display_name
+            }))
+        template = self.env.ref("account.email_template_edi_invoice", False)
+        attachment_ids_ids = \
+            [attachment_id.id for attachment_id in attachment_ids]
+        template.attachment_ids = [(6, 0, attachment_ids_ids)]
+        return super(AccountInvoice, self).action_invoice_sent()
 
 
 class AccountInvoiceLine(models.Model):
